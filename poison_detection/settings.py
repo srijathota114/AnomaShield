9aa57b05997eedd9607760eda7e3cc5e3620ef53
srijathota114 <thotasrija748@gmail.com>
2026-02-24 22:17:10 +0530

    fixed allowed hosts for render

--- poison_detection/settings.py
+++ poison_detection/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-your-secret-key-here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['anomashield.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
